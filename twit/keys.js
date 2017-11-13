608c2fb64e45bcd1787df48adf8756c760854ca1
hagybrian <brianhagy@gmail.com>
2017-11-13 14:09:36 -0600

    Add files via upload

---
+++ twit/keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'eq9WIOeEvRHi8AnC5O9ZdV1Ks',
+  consumer_secret: '45YxTXPo8FdwY3xdfwAeVvWT9rI9mYFqwVFtFPwEdW5PFMKoIR',
+  access_token_key: '2425753862-4HaPmIVnkV6XCw78u6tSP2nbqCcPv56sMDPXRiS',
+  access_token_secret: '9V3XhsQv6TnHaK6DiDtzWTj2aVGWutiHkWg4hWvPtoZoI',
+}
+
+module.exports = twitterKeys;
